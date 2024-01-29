94db30982cee3f4faca9c8d7ff18a496be3b7de9
RabiaNoumanSiddiq <rabia.nouman@educative.io>
2024-01-29 17:11:15 +0500

    initial commit

---
+++ composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vendor/autoload.php',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'smarty/smarty' => array(
+            'pretty_version' => 'v4.3.5',
+            'version' => '4.3.5.0',
+            'reference' => 'e0cbbdf6ea21768d0194e59d2f8c2e20d5f0868c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../smarty/smarty',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'vendor/autoload.php' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
